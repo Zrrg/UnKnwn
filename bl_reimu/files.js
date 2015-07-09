ecf2a5e62714c11f2d4d55fb8b2009db5f48b0e5
Zrrg <hiroaritillwhen@gmail.com>
2015-07-09 16:37:24 +0300

    Added th11 sprites

--- bl_reimu/files.js
+++ bl_reimu/files.js
@@ -1,6 +1,6 @@
 {
-	"README.MD": 164656127,
-	"patch.js": 89398789,
+	"README.MD": 1567871180,
+	"patch.js": 2121990420,
 	"th10/face/pl00/face_pl00an_u.png": 1469980073,
 	"th10/face/pl00/face_pl00dp_u.png": 2050956969,
 	"th10/face/pl00/face_pl00hp_u.png": 1248729709,
@@ -12,5 +12,16 @@
 	"th10/face/pl00/face_pl00sw_u.png": 2926988695,
 	"th10/player/pl00/pl00.png": 2969028437,
 	"th10/title/sl_pl00.png": 2762743651,
-	"th10/title/title00a.png": 1289062927
+	"th10/title/title00a.png": 1289062927,
+	"th11/face/pl00/face_pl00an.png": 1648764054,
+	"th11/face/pl00/face_pl00dp.png": 454430992,
+	"th11/face/pl00/face_pl00hp.png": 3901645518,
+	"th11/face/pl00/face_pl00n2.png": 1244738299,
+	"th11/face/pl00/face_pl00no.png": 299891401,
+	"th11/face/pl00/face_pl00pr.png": 45241133,
+	"th11/face/pl00/face_pl00sp.png": 47051410,
+	"th11/face/pl00/face_pl00sw.png": 1713734088,
+	"th11/player/pl00/pl00.png": 3663168342,
+	"th11/title/sl_pl00.png": 4166729279,
+	"th11/title/title00a.png": 674641006
 }
--- bl_reimu/patch.js
+++ bl_reimu/patch.js
@@ -6,5 +6,5 @@
 	"servers": [
 		"https://raw.githubusercontent.com/Zrrg/UnKnwn/master/bl_reimu/"
 	],
-	"title": "BlueReimu: Sprite and portraits replacement. (th10)"
+	"title": "BlueReimu: Sprite and portraits replacement. (10,11)"
 }
