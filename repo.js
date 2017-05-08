ce266f61f2e0c4adf5cd75ae8283c0f4e755229c
UnKnwn <hiroaritillwhen@gmail.com>
2017-05-08 08:30:48 +0300

    how do i do this

--- repo.js
+++ repo.js
@@ -2,9 +2,8 @@
 	"contact": "hiroaritillwhen@gmail.com",
 	"id": "UnKnwn",
 	"patches": {
-		"bl_reimu": "BlueReimu: Sprite and portraits replacement. (Th10+)",
-		"untanned_cirno": "UntannedCirno"
-	},
+		"bl_reimu": "BlueReimu: Sprite and portraits replacement. (Th10+)"
+, "untanned_cirno":"Replaces tanned Cirno with just Cirno (TH16) [made by xJeePx]"	},
 	"servers": [
 		"https://raw.githubusercontent.com/Zrrg/UnKnwn/master/"
 	],
